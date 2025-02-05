40d7dd7557d8b49dc27c0964e42a00036c0ad938
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2025-02-06 01:05:11 +0700

    feat: support nestjs

--- src/lib/core/Nest/index.ts
+++ src/lib/core/Nest/index.ts
@@ -1,29 +1,21 @@
-import { Model, Repository } from "..";
+import { Model, Repository, TypeOfRepository } from "..";
+
+const nameOfModel = <M extends Model>(model : new () => M): string  => {
+  
+  return `${new model().constructor?.name ?? Math.random().toString(36).slice(2)}@Repository`
+}
 
 export type NestRepository<M extends Model> =  ReturnType<typeof Repository<M>>
 
-export const NestInject =< M extends Model>(model : string | (new () => M)) => {
-  return (target: object, key: string) => {
-     
-    const type = typeof model === 'string' 
-      ? model 
-      : `${new model().constructor?.name ?? Math.random().toString(36).slice(2)}Inject` 
-
-    const getMeta = Reflect.getMetadata('self:properties_metadata', target.constructor) ?? []
-
-    const properties = [...getMeta, { key, type }]
-
-    Reflect.defineMetadata(
-      'self:properties_metadata',
-      properties,
-      target.constructor,
-    )
-  }
+export const NestInject = < M extends Model>( model : string | (new () => M)) => {
+  return typeof model === 'string' 
+    ? model 
+    : nameOfModel(model)
 }
 
 export const NestProvider = <M extends Model>(model : new () => M) => {
   return { 
-    provide: `${new model().constructor?.name ?? Math.random().toString(36).slice(2)}Repository`, 
-    useValue:  Repository<M>(model) as any
+    provide: nameOfModel(model), 
+    useValue:  Repository<M>(model) as unknown
   }
 }
