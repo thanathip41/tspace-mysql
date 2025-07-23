1ce02ec98f12523cf68ff56f78add707eed3fe74
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2025-07-23 11:23:39 +0700

    fix: covertDateToDateString check regex only format datetime

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -97,7 +97,7 @@ const covertDateToDateString = (data : any) => {
 
     const isISODateString = (s: unknown): boolean => {
         if (typeof s !== 'string') return false;
-        const isoRegex = /^\d{4}-\d{2}-\d{2}(?:[ T]\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})?)?$/;
+        const isoRegex = /^\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})?$/;
 
         return isoRegex.test(s) && !isNaN(Date.parse(s.replace(' ', 'T')));
     };
